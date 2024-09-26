5a40a117d50e384df965a8ec7e1b14f8785b2fa2
Dacron <favouradebimpe63@gmail.com>
2024-09-26 23:45:53 +0100

    Added functionality to SingleBlog component to fetch and display individual blog posts, including images and block content, and created custom serializers for styling block content.

--- src/Pages/SingleBlog.jsx
+++ src/Pages/SingleBlog.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Box, Typography, Stack } from "@mui/material";
+import { blogStore } from "../Store/blogStore";
+import imageUrlBuilder from "@sanity/image-url";
+import sanityClient from "../client";
+import SanityBlockContent from "@sanity/block-content-to-react";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
 
 function SingleBlog() {
   const { slug } = useParams();
+  const { singlePost, fetchSingleBlog, customSerializers } = blogStore();
+
+  useEffect(() => {
+    fetchSingleBlog(slug);
+  }, [slug, fetchSingleBlog]);
+
+  if (!singlePost) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#f2ecff",
+          height: "100vh",
+          color: "black",
+        }}
+      >
+        <Typography variant="h6">Loading...</Typography>
+      </Box>
+    );
+  }
+
   return (
     <Box
       sx={{
@@ -11,8 +43,31 @@ function SingleBlog() {
         alignItems: "center",
         backgroundColor: "#f2ecff",
         height: "100vh",
+        color: "black",
       }}
-      >
+    >
+      <Stack>
+        {" "}
+        <Typography variant="p">
+          <b>{singlePost?.title}</b>
+        </Typography>
+        <Box
+          component="img"
+          sx={{
+            width: "100%",
+            height: "auto",
+          }}
+          src={singlePost.mainImage.asset.url}
+          alt={singlePost.mainImage.alt}
+          loading="lazy"
+        />
+        <SanityBlockContent
+          blocks={singlePost.body}
+          projectId="dtbj3t1s"
+          dataset="production"
+          serializers={customSerializers}
+        />
+      </Stack>
     </Box>
   );
 }
--- src/Store/blogStore.jsx
+++ src/Store/blogStore.jsx
@@ -1,11 +1,12 @@
 import { create } from "zustand";
 import sanityClient from "../client";
 
+import { Box, Typography, Stack } from "@mui/material";
+
 export const blogStore = create((set) => ({
   blogPost: null,
 
   //for the fetaured posts
-
   fetchFeaturedBlogPosts: async () => {
     try {
       const query = `*[_type == "post"] | order(_createdAt asc) [0...5]{
@@ -48,4 +49,75 @@ export const blogStore = create((set) => ({
       console.error("Error fetching blog posts:", error);
     }
   },
+
+  //logic for single blogs
+  singlePost: null,
+  setSinglePost: (singlePost) => set({ singlePost }),
+  fetchSingleBlog: async (slug) => {
+    try {
+      const query = `*[slug.current == "${slug}"]{
+            title,
+            _id,
+            slug,
+            author ->{
+                name,
+                _id,
+                nickname
+              },
+            mainImage{
+                asset -> {
+                    _id,
+                    url
+                }
+            },
+            body,
+            "name": author-> name,
+            "authorImage": author-> image
+        }`;
+      const data = await sanityClient.fetch(query, { slug });
+
+      setTimeout(() => set({ singlePost: data[0] }), 1000);
+
+      if (data[0]?.title) {
+        document.title = data[0].title;
+      }
+    } catch (error) {
+      console.error("Error fetching blog posts:", error);
+    }
+  },
+
+  // STYLING THE CONTENT IN BLOCK CONTENT
+  customSerializers: () => ({
+    types: {
+      image: ({ node }) => {
+        const imageUrl = builder.image(node.asset).width(400).url(); // Adjust width as needed
+        return (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Box
+              component="img"
+              src={imageUrl}
+              alt={node.alt}
+              sx={{
+                width: { xs: "320px", sm: "450px", lg: "900px" },
+                height: "auto",
+              }}
+            />
+          </Box>
+        );
+      },
+      block: ({ children }) => (
+        <Typography
+          variant="h2" // You can adjust the variant based on your design
+          sx={{ textAlign: "justify", margin: "10px 0", color: "pink" }} // Add the style to justify the text
+        >
+          {children}
+        </Typography>
+      ),
+    },
+  }),
 }));
